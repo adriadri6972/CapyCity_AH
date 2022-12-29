93c3fdee18fb3bc7a12ad50d2b08a16660f6416c
adriadri6972 <adrian.haefner@t-online.de>
2022-12-29 15:06:47 +0100

    Ende Kapitel 3

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "CapyCitySim.h"
 
+// alle Aufgaben von Kapitel 3 habe ich ausversehen während Kapitel 2 schon erledigt 
+// (bei der Optimierung während ich auf die Folgekapitel gewartet habe)
+// ich verwalte meine Materialen als map über die Gebäude (Carlas Idee)
+
+
 int main()
 {
 	CapyCitySim a = CapyCitySim();
-	CapyCitySim b = CapyCitySim();
-	CapyCitySim c = CapyCitySim();
-	CapyCitySim d = CapyCitySim();
+	//CapyCitySim b = CapyCitySim();
+	//CapyCitySim c = CapyCitySim();
+	//CapyCitySim d = CapyCitySim();
 
 	while (true) {
 		a.menue();
